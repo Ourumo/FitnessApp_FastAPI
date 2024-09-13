e2488e4c71861259c1f341690b6cf7687d6bdd02
Ourumo <ss8638@naver.com>
2024-09-13 19:10:14 +0900

    refactor: models 수정

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String)
+    email = Column(String, unique=True, index=True)
     name = Column(String)
     profile_img = Column(LargeBinary)
     
