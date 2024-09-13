4a3a8a185ae740e17b48e04fd7c6079016daa29b
Ourumo <ss8638@naver.com>
2024-09-13 23:17:21 +0900

    feat: 이메일 중복 확인 추가

--- crud.py
+++ crud.py
@@ -3,26 +3,22 @@ from . import models, schemas
 
 # 화원가입
 def regiseter(db: Session, user: schemas.UserRegister):
-    checkUser = db.query(models.User).filter(models.User.email == user.email).first()
-    if checkUser is None:
-        return None
-    else:
-        db_user = models.User(
-            email=user.email,
-            name=user.name,
-        )
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-        return db_user
+    db_user = models.User(
+        email=user.email,
+        name=user.name
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# 이메일 중복 확인
+def check_email(db: Session, user: schemas.UserCheckEmail):
+    return db.query(models.User).filter(models.User.email == user.email).first()
 
 # 로그인
 def login(db: Session, user: schemas.UserLogin):
-    db_user = db.query(models.User).filter(
+    return db.query(models.User).filter(
         models.User.email == user.email,
         models.User.password == user.password
     ).first()
-    if db_user is None:
-        return None
-    else:
-        return db_user
