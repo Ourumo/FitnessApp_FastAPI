e12219f6fb4c5c344007738ce6997c7aaf54e385
Ourumo <ss8638@naver.com>
2024-09-13 23:47:09 +0900

    refactor: 디렉토리 구조 변경

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from app import crud, models, schemas
+from app.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# 의존성
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/register")
+def register(user: schemas.UserRegister, db: Session = Depends(get_db)):
+    db_user = crud.check_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="이메일 중복")
+    return crud.regiseter(db=db, user=user)
+
+@app.get("/login")
+def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    db_user = crud.login(db, user=user)
+    if db_user is None:
+        raise HTTPException(status_code=400, detail="아이디나 비밀번호 틀림")
+    return db_user
